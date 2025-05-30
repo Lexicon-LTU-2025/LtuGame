dc3c03f75e325198396d5ee083524776d77eff9c
DZM77 <32109758+DZM77@users.noreply.github.com>
2025-05-30 13:22:00 +0200

    Add project files.

---
+++ LtuGame.ConsoleGame/Game.cs
@@ -0,0 +1,19 @@
+﻿
+using System.ComponentModel;
+
+internal class Game
+{
+    public Game()
+    {
+    }
+
+    internal void Run()
+    {
+        Init();
+    }
+
+    private void Init()
+    {
+        var map = new Map(height: 10, width: 10);
+    }
+}

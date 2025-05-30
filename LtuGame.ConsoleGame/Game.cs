85af2fc26d78781da9a8b4893f76e4753d8ecbff
DZM77 <32109758+DZM77@users.noreply.github.com>
2025-05-30 13:44:35 +0200

    Game Play

--- LtuGame.ConsoleGame/Game.cs
+++ LtuGame.ConsoleGame/Game.cs
@@ -3,16 +3,39 @@ using System.ComponentModel;
 
 internal class Game
 {
-    private Map map;
-    private Player player;
+    private Map map = null!;
+    private Player player = null!;
 
     public Game()
     {
+
     }
 
     internal void Run()
     {
         Init();
+        Play();
+    }
+
+    private void Play()
+    {
+       bool gameInProgress = true;
+
+        do
+        {
+            //Drawmap
+
+            //GetCommand
+
+            //Act
+
+            //Drawmap
+
+            //Ememyaction
+
+            //Drawmap
+
+        }while (gameInProgress);
     }
 
     private void Init()

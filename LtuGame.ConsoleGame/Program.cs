5686d8dba123ce3993f205907043bb0d2905f423
DZM77 <dimitris.bjorlingh@outlook.com>
2025-06-09 10:02:24 +0200

    Comment out in Program

--- LtuGame.ConsoleGame/Program.cs
+++ LtuGame.ConsoleGame/Program.cs
@@ -2,36 +2,36 @@
 using LtuGame.ConsoleGame.Extensions;
 using LtuGame.LimitedList;
 
-IEnumerable<int> list = new List<int>();
-IEnumerable<Creature> list2 = new Creature[25];
-//list2.CreatureAt(new Cell(new LtuGame.ConsoleGame.GameWorld.Position(1,2)));
+//IEnumerable<int> list = new List<int>();
+//IEnumerable<Creature> list2 = new Creature[25];
+////list2.CreatureAt(new Cell(new LtuGame.ConsoleGame.GameWorld.Position(1,2)));
 
 
 
-LimitedList<int> lm = new LimitedList<int>(10);
+//LimitedList<int> lm = new LimitedList<int>(10);
 
 
 
 
 
-var li = new List<int>(10);
+//var li = new List<int>(10);
 
-var str = "adafasgfsge";
+//var str = "adafasgfsge";
 
-foreach (var item in str)
-{
-    Console.WriteLine(item);
-}
+//foreach (var item in str)
+//{
+//    Console.WriteLine(item);
+//}
 
-foreach (var item in li)
-{
-    Console.WriteLine(item);
-}
+//foreach (var item in li)
+//{
+//    Console.WriteLine(item);
+//}
 
-foreach (var item in lm)
-{
-    Console.WriteLine(item);
-}
+//foreach (var item in lm)
+//{
+//    Console.WriteLine(item);
+//}
 
 
 

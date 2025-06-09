0cffac2871e1d7774cfe3fc0d98f7609a4dbab70
DZM77 <dimitris.bjorlingh@outlook.com>
2025-06-09 14:49:56 +0200

    Add Stats

--- LtuGame.ConsoleGame/Game.cs
+++ LtuGame.ConsoleGame/Game.cs
@@ -103,6 +103,7 @@ internal class Game
     {
          ConsoleUI.Clear();
          ConsoleUI.Draw(_map);
+         ConsoleUI.PrintStats($"Health: {_player.Health}");
          ConsoleUI.PrintLog();
 
         

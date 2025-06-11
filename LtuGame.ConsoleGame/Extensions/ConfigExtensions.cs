95f73b43bd128416c5c1758ddea9009894d7e9d8
DZM77 <dimitris.bjorlingh@outlook.com>
2025-06-11 14:34:13 +0200

    Start on test Extension methods

--- LtuGame.ConsoleGame/Extensions/ConfigExtensions.cs
+++ LtuGame.ConsoleGame/Extensions/ConfigExtensions.cs
@@ -14,3 +14,26 @@ public static class ConfigExtensions
         return int.TryParse(section[key], out int result) ? result : 0;
     }
 }
+
+public static class GetMapSizeForClass
+{
+    public static IGetMapSizeFor Implementation { private get; set; } = new GetMapSizeFor();
+    public static int GetMapSizeFor2(this IConfiguration config, string key)
+    {
+        return Implementation.GetMapSizeFor2(config, key);
+    }
+}
+
+public interface IGetMapSizeFor
+{
+    int GetMapSizeFor2(IConfiguration config, string key);
+}
+
+public class GetMapSizeFor : IGetMapSizeFor
+{
+    public int GetMapSizeFor2(IConfiguration config, string key)
+    {
+        var section = config.GetSection("game:mapsettings");
+        return int.TryParse(section[key], out int result) ? result : 0;
+    }
+}
